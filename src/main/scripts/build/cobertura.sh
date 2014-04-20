317bfb8d31a35bc2aafd66219a46b77dbbade24a
Jesús López Alonso <jeslopalo@sandbox.es>
2014-04-20 08:02:49 +0200

    Setup of coveralls-maven-plugin execution
    Signed-off-by: Jesús López Alonso <jeslopalo@sandbox.es>

--- src/main/scripts/build/cobertura.sh
+++ src/main/scripts/build/cobertura.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Generating cobertura reports to be submited to coverall service"
-mvn test cobertura:cobertura coveralls:cobertura -P coveralls
+mvn test cobertura:cobertura coveralls:cobertura
