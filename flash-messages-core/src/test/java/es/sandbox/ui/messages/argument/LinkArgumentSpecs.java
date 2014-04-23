1159a40b258402afb081f504757852ef5b6968e4
Jesús López Alonso <jeslopalo@sandbox.es>
2014-04-23 22:50:31 +0200

    - Increasing code coverage for flash-messages-taglibs module

--- flash-messages-core/src/test/java/es/sandbox/ui/messages/argument/LinkArgumentSpecs.java
+++ flash-messages-core/src/test/java/es/sandbox/ui/messages/argument/LinkArgumentSpecs.java
@@ -14,25 +14,25 @@ import org.junit.runner.RunWith;
 @RunWith(Enclosed.class)
 public class LinkArgumentSpecs {
 
-	public static class UrlSpecs {
+   public static class UrlSpecs {
 
-		@Test
-		public void it_should_fail_with_invalid_url() {
-			assertThatIn(LinkArgument.class)
-					.constructorWithArguments(String.class)
-					.willThrow(NullPointerException.class)
-					.invokedWith(nullArgument());
+      @Test
+      public void it_should_fail_with_invalid_url() {
+         assertThatIn(LinkArgument.class)
+               .constructor(String.class)
+               .willThrowNullPointerException()
+               .invokedWith(nullArgument());
 
-			assertThatIn(LinkArgument.class)
-					.constructorWithArguments(String.class)
-					.willThrow(IllegalArgumentException.class)
-					.invokedWith(empty())
-					.invokedWith(blank());
-		}
+         assertThatIn(LinkArgument.class)
+               .constructor(String.class)
+               .willThrowIllegalArgumentException()
+               .invokedWith(empty())
+               .invokedWith(blank());
+      }
 
-		@Test
-		public void it_should_be_constructed_with_an_url() {
-			assertThat(new LinkArgument("/an/url")).isNotNull();
-		}
-	}
+      @Test
+      public void it_should_be_constructed_with_an_url() {
+         assertThat(new LinkArgument("/an/url")).isNotNull();
+      }
+   }
 }
