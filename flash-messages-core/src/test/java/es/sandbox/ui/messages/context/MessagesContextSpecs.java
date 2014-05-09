d4213fa72217133f73dada9d4b31587eb6873729
Jesús López Alonso <jeslopalo@sandbox.es>
2014-05-09 17:29:29 +0200

    flash-messages-core: 100% code coverage!

--- flash-messages-core/src/test/java/es/sandbox/ui/messages/context/MessagesContextSpecs.java
+++ flash-messages-core/src/test/java/es/sandbox/ui/messages/context/MessagesContextSpecs.java
@@ -16,6 +16,8 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
 import es.sandbox.ui.messages.Level;
+import es.sandbox.ui.messages.Messages;
+import es.sandbox.ui.messages.MessagesPublisher;
 import es.sandbox.ui.messages.resolver.MessageResolverStrategy;
 import es.sandbox.ui.messages.resolver.StringFormatMessageResolverStrategy;
 import es.sandbox.ui.messages.store.MessagesStore;
@@ -108,4 +110,79 @@ public class MessagesContextSpecs {
          verify(this.mockMessagesStoreAccessor, never()).put(any(MessagesStore.class));
       }
    }
+
+   public static class PublisherSpecs {
+
+      private MessagesStoreAccessorFactory mockMessagesStoreAccessorFactory;
+      private HttpServletRequest mockHttpServletRequest;
+      private MessagesStoreAccessor mockMessagesStoreAccessor;
+      private MessagesContext sut;
+
+
+      @Before
+      public void setup() {
+         this.mockMessagesStoreAccessorFactory= mock(MessagesStoreAccessorFactory.class);
+         this.mockHttpServletRequest= mock(HttpServletRequest.class);
+         this.mockMessagesStoreAccessor= mock(MessagesStoreAccessor.class);
+
+         this.sut= new MessagesContext(this.mockMessagesStoreAccessorFactory, new StringFormatMessageResolverStrategy());
+      }
+
+      @Test(expected= NullPointerException.class)
+      public void it_should_raise_an_exception_with_null_request() {
+         this.sut.publisher(null);
+      }
+
+      @Test
+      public void it_should_return_a_publisher() {
+         given(this.mockMessagesStoreAccessorFactory.create(this.mockHttpServletRequest)).willReturn(this.mockMessagesStoreAccessor);
+         given(this.mockMessagesStoreAccessor.get()).willReturn(new MessagesStore());
+
+         final Messages messages= this.sut.publisher(this.mockHttpServletRequest);
+
+         assertThat(messages).isInstanceOf(MessagesPublisher.class);
+      }
+   }
+
+   public static class LevelMessagesSpecs {
+
+      private MessagesStoreAccessorFactory mockMessagesStoreAccessorFactory;
+      private HttpServletRequest mockHttpServletRequest;
+      private MessagesStoreAccessor mockMessagesStoreAccessor;
+      private MessagesContext sut;
+
+
+      @Before
+      public void setup() {
+         this.mockMessagesStoreAccessorFactory= mock(MessagesStoreAccessorFactory.class);
+         this.mockHttpServletRequest= mock(HttpServletRequest.class);
+         this.mockMessagesStoreAccessor= mock(MessagesStoreAccessor.class);
+
+         this.sut= new MessagesContext(this.mockMessagesStoreAccessorFactory, new StringFormatMessageResolverStrategy());
+      }
+
+      @Test(expected= NullPointerException.class)
+      public void it_should_raise_an_exception_with_null_level() {
+         given(this.mockMessagesStoreAccessorFactory.create(this.mockHttpServletRequest)).willReturn(this.mockMessagesStoreAccessor);
+         given(this.mockMessagesStoreAccessor.get()).willReturn(new MessagesStore());
+
+         this.sut.levelMessages(null, this.mockHttpServletRequest);
+      }
+
+      @Test(expected= NullPointerException.class)
+      public void it_should_raise_an_exception_with_null_request() {
+         this.sut.levelMessages(Level.SUCCESS, null);
+      }
+
+      @Test
+      public void it_should_return__messages_by_level() {
+         given(this.mockMessagesStoreAccessorFactory.create(this.mockHttpServletRequest)).willReturn(this.mockMessagesStoreAccessor);
+         given(this.mockMessagesStoreAccessor.get()).willReturn(new MessagesStore());
+
+         for (final Level level : Level.values()) {
+            assertThat(this.sut.levelMessages(level, this.mockHttpServletRequest)).isEmpty();
+         }
+      }
+   }
+
 }
--- flash-messages-core/src/test/java/es/sandbox/ui/messages/store/MessagesStoreSpecs.java
+++ flash-messages-core/src/test/java/es/sandbox/ui/messages/store/MessagesStoreSpecs.java
@@ -14,47 +14,83 @@ import es.sandbox.ui.messages.MessageFixtureGenerator;
 @RunWith(Enclosed.class)
 public class MessagesStoreSpecs {
 
-	public static class ClearingSpecs {
+   public static class ContainsMessagesByLevelSpecs {
 
-		private MessagesStore sut;
-
-
-		@Before
-		public void setup() {
-			this.sut= new MessagesStoreFixtureGenerator().generate();
-		}
-
-		@Test
-		public void it_should_clear_messages() {
-			this.sut.clear();
-
-			for (final Level level : Level.values()) {
-				assertThat(this.sut.containsMessages(level)).isFalse();
-			}
-		}
-	}
-
-	public static class ToStringSpecs {
-
-		private MessageFixtureGenerator messagesFixturer;
-		private MessagesStore sut;
+      private MessagesStore sut;
 
 
-		@Before
-		public void setup() {
-			this.messagesFixturer= MessageFixtureGenerator.fixturer();
-			this.sut= new MessagesStore();
-			this.sut.add(this.messagesFixturer.generateDefault());
-		}
+      @Before
+      public void setup() {
+         this.sut= new MessagesStoreFixtureGenerator()
+               .error(1)
+               .warning(1)
+               .success(1)
+               .info(1)
+               .generate();
+      }
 
-		@Test
-		public void it_should_include_messages_in_to_string() {
-			assertThat(this.sut.toString()).isEqualTo("messages [{SUCCESS=[], INFO=[[(2010-08-19 14:30:00) INFO: This is an example of message]], WARNING=[], ERROR=[]}]");
-		}
+      @Test(expected= NullPointerException.class)
+      public void it_should_fail_without_level() {
+         new MessagesStore().containsMessages(null);
+      }
 
-		@Test
-		public void it_should_include_empty_with_empty_store() {
-			assertThat(new MessagesStore().toString()).isEqualTo("messages [empty]");
-		}
-	}
+      @Test
+      public void it_should_does_not_contain_messages() {
+         final MessagesStore store= new MessagesStore();
+         for (final Level level : Level.values()) {
+            assertThat(store.containsMessages(level)).isFalse();
+         }
+      }
+
+      @Test
+      public void it_should_contains_messages() {
+         for (final Level level : Level.values()) {
+            assertThat(this.sut.containsMessages(level)).isTrue();
+         }
+      }
+   }
+
+   public static class ClearingSpecs {
+
+      private MessagesStore sut;
+
+
+      @Before
+      public void setup() {
+         this.sut= new MessagesStoreFixtureGenerator().generate();
+      }
+
+      @Test
+      public void it_should_clear_messages() {
+         this.sut.clear();
+
+         for (final Level level : Level.values()) {
+            assertThat(this.sut.containsMessages(level)).isFalse();
+         }
+      }
+   }
+
+   public static class ToStringSpecs {
+
+      private MessageFixtureGenerator messagesFixturer;
+      private MessagesStore sut;
+
+
+      @Before
+      public void setup() {
+         this.messagesFixturer= MessageFixtureGenerator.fixturer();
+         this.sut= new MessagesStore();
+         this.sut.add(this.messagesFixturer.generateDefault());
+      }
+
+      @Test
+      public void it_should_include_messages_in_to_string() {
+         assertThat(this.sut.toString()).isEqualTo("messages [{SUCCESS=[], INFO=[[(2010-08-19 14:30:00) INFO: This is an example of message]], WARNING=[], ERROR=[]}]");
+      }
+
+      @Test
+      public void it_should_include_empty_with_empty_store() {
+         assertThat(new MessagesStore().toString()).isEqualTo("messages [empty]");
+      }
+   }
 }
