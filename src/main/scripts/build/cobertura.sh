9186c04023f4bae9c3f9ac854b4b16408fb9c0bd
Jesús López Alonso <jeslopalo@sandbox.es>
2016-10-28 23:20:20 +0200

    Fix 'pwd' command invocation from cobertura.sh

--- src/main/scripts/build/cobertura.sh
+++ src/main/scripts/build/cobertura.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Working directory: [${pwd}]"
+echo "Working directory: [$(pwd)]"
 
 echo "Generating cobertura reports to be submited to coverall service"
 mvn cobertura:cobertura coveralls:report -P coveralls "$1"
